050d465dd1f8838b57b90cd94ca871002c8a6c4e
Atif Afzal <atif.afzal@paytm.com>
2016-12-02 15:36:37 +0530

    using sass-thematic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,61 @@
 /* @flow */
-import data from './data'
+import thematic from 'sass-thematic';
+import path from 'path';
+import util from 'util';
+import fp from 'lodash/fp';
+import Debug from 'debug';
 
-function add (a: number, b: number): number {
-  return a + b
-}
+const debug = Debug('import-style');
 
-console.log(`${add(data, 2)}`)
+const ast = thematic.parseASTSync({
+  file: path.resolve(__dirname, '../data/foo.scss')
+});
+
+type NodeType = {
+  type: string,
+  content: Array<NodeType> | string,
+  syntax: string,
+  start: {
+    line: number,
+    column: number,
+  },
+  end: {
+    line: number,
+    column: number,
+  },
+};
+
+const getStyleClasses = (Node: NodeType): Array<string> => {
+  const classes: Array<NodeType> = [];
+
+  traverse(ast);
+
+  function traverse (Node: NodeType) {
+    if (!Array.isArray(Node.content)) {
+      return;
+    }
+
+    if (Node.type === 'class') {
+      classes.push(Node);
+      return;
+    }
+
+    for (let i = 0; i < Node.content.length; ++i) {
+      traverse(Node.content[i]);
+    }
+  }
+
+  const getClassNames = fp.compose(
+    fp.flatMap('content'),
+    fp.filter({ type: 'ident' }),
+    fp.flatMap('content'),
+  );
+
+  debug(util.inspect(classes, {}, null));
+
+  return getClassNames(classes);
+};
+
+const classes = getStyleClasses(ast);
+
+console.log(classes);
